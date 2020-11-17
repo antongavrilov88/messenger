5b00306241649205f56b27bcd6b8df80d63459a6
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2020-11-17 22:22:08 +0300

    fix server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.use(express.static('./static'));
+app.use(express.static(`${__dirname}/static`));
+
+
+app.use('/', function (request, response) {
+    response.send('<h1>Главная страница</h1>')
+  })
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
