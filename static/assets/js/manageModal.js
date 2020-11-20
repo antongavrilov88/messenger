dbfbc295ba4e458e8fa74b0aab216cc97d0916a6
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2020-11-21 02:13:36 +0300

    add modal

---
+++ static/assets/js/manageModal.js
@@ -0,0 +1,17 @@
+function openModal( modalID ) {
+
+    let modal = document.getElementById(modalID)
+
+    modal.hidden = false
+
+    return
+}
+
+function closeModal( modalID ) {
+
+    let modal = document.getElementById(modalID)
+
+    modal.hidden = true
+
+    return
+}
