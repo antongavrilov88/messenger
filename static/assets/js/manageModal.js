8b469b944107beac7c22aeb8f33273f2162370eb
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2020-11-27 00:26:48 +0300

    fix css/chat.css

--- static/assets/js/manageModal.js
+++ static/assets/js/manageModal.js
@@ -1,14 +1,14 @@
-function openModal(modalID) {
-    let modal = document.getElementById(modalID)
+function openModal( modalID ) {
+    let modal = document.getElementById( modalID )
     if (modal) {
         modal.hidden = false
     }
     return false
 }
 
-function closeModal(modalID) {
-    let modal = document.getElementById(modalID)
-    if (modal) {
+function closeModal( modalID ) {
+    let modal = document.getElementById( modalID )
+    if ( modal ) {
         modal.hidden = true
     }
     return false
