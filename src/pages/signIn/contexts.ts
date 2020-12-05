15d6b9faa594c6e9cc6e0e8decf96dc3c98bf888
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2020-12-06 00:39:30 +0300

    add contexts files

---
+++ src/pages/signIn/contexts.ts
@@ -0,0 +1,29 @@
+export const formCTX = {
+    className: "form-container",
+    id: "loginForm",
+    title: "Авторизация",
+    inputs: [
+        {
+            lable: {
+                className: "form__input__name",
+                title: "ЛОГИН"
+            },
+            input: {
+                className: "form__input",
+                type: "text",
+                name: "login"
+            }
+        },
+        {
+            lable: {
+                className: "form__input__name",
+                title: "ПАРОЛЬ"
+            },
+            input: {
+                className: "form__input",
+                type: "password",
+                name: "password"
+            }
+        }
+    ]
+    }
--- static/assets/js/pages/signIn/index.js
+++ static/assets/js/pages/signIn/index.js
@@ -1,35 +1,7 @@
 import { render } from '../../utils/render.js';
 import SignIn from './SignIn.js';
 import Form from '../../components/form/Form.js';
-const formCTX = {
-    className: "form-container",
-    id: "loginForm",
-    title: "Авторизация",
-    inputs: [
-        {
-            lable: {
-                className: "form__input__name",
-                title: "ЛОГИН"
-            },
-            input: {
-                className: "form__input",
-                type: "text",
-                name: "login"
-            }
-        },
-        {
-            lable: {
-                className: "form__input__name",
-                title: "ПАРОЛЬ"
-            },
-            input: {
-                className: "form__input",
-                type: "password",
-                name: "password"
-            }
-        }
-    ]
-};
+import { formCTX } from './contexts';
 const form = new Form(formCTX);
 const signInChildren = [
     {
