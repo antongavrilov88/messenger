d92659b12a029f3f7c8ac06650e56379644e7454
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2020-12-12 17:03:26 +0300

    fix changePassword issue

--- src/components/changePassword/ChangePassword.ts
+++ src/components/changePassword/ChangePassword.ts
@@ -1,12 +1,17 @@
 import Block from '../../utils/Block.js'
+import formHandler from '../../utils/manageForm.js'
+import validateInput from '../../utils/validateInput.js'
 import { tpl } from './template.js'
-import { ChangePasswordProps } from './types.js'
 
 class ChangePassword extends Block {
     constructor(props: object | undefined) {
         super("div", props)
     }
 
+    componentDidMount() {
+        window.validateInput = validateInput
+        window.formHandler = formHandler
+    }
     render() {
         return this.compile(tpl, {
             formHandler: this.props ? this.props.formHandler : null,
