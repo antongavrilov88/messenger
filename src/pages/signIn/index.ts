ec3cc24c1b314112a35777e309d10240580551d2
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2020-12-23 00:56:30 +0300

    fixing store case

--- src/pages/signIn/index.ts
+++ src/pages/signIn/index.ts
@@ -1,8 +1,26 @@
 import { render } from '../../utils/render.js'
 import SignIn from './SignIn.js'
 import formHandler from '../../utils/manageForm.js'
+import Store from '../../utils/Store.js'
+import UnauthWorkspace from '../../components/unauthWorkSpace/UnauthWorkspace.js'
+import Form from '../../components/form/Form.js'
+import { formCTX } from './contexts.js'
 
 const signIn = new SignIn()
+
+let store = Store
+
+function stateToProps(state: { auth: { "title": any } }) {
+    this.setProps({content: new UnauthWorkspace({
+        content: new Form( {...formCTX, title: state.auth.title} )
+    })})
+}
+
+store.subscribe(stateToProps)
+
+setTimeout(() => {
+    store.state.auth.title = 'PISA'
+}, 3000);
 
 render(".app", signIn)
 let form = document.getElementById( 'loginForm' )
--- src/utils/Store.ts
+++ src/utils/Store.ts
@@ -1,8 +1,70 @@
-// import EventBus from './eventBus.js'
+import EventBus from './eventBus.js'
 
-// class Store {
-//     static EVENTS = {
-//         FLOW_SDU: "flow:store-did-update"
-//     }
+class Store {
+    protected static _instance: Store = new Store
+    static EVENTS = {
+        FLOW_SDU: "flow:store-did-update",
+        FLOW_SNSTP: "flow:set-new-state-to-props"
+    };
+    eventBus: () => EventBus;
+    state: {
+        [key: string]: any
+    } = {auth: { title: ''} }
+    static state: any;
+    static eventBus: any;
+    constructor(state = {}) {
+        if (Store._instance) {
+            throw new Error('Can not create new instance')
+        }
+        const eventBus = new EventBus
+        this.eventBus = () => eventBus
+        this._registerEvents(eventBus)
+        this.state = this._makeStateProxy(state)
+    }
 
-// }
+    static getState(): { auth: { "title": any } } {
+        return this.state
+    }
+
+    _registerEvents(eventBus: EventBus) {
+        eventBus.on(Store.EVENTS.FLOW_SDU, this._storeDidUpdate.bind(this));
+    }
+
+    static subscribe(callback: (state: any) => void) {
+        this.eventBus().on(Store.EVENTS.FLOW_SNSTP, callback)
+    }
+
+    _storeDidUpdate(oldState = {}, newState = {}) {
+        // organize dictionary of callbacks and think of triggering them
+        // trigger all observers
+        const response = this.storeDidUpdate(oldState, newState);
+        if (!response) {
+            return;
+        }
+        this.eventBus().emit(Store.EVENTS.FLOW_SNSTP, this.state)
+    }
+
+    storeDidUpdate(oldState: {}, newState: {}) {
+        // add isEqual function
+        return true
+    }
+
+    _makeStateProxy(obj: {}) {
+        const self = this;
+        return new Proxy(obj, {
+            get(target: { [prop: string]: any }, prop: any) {
+                const value = target[prop];
+                return typeof value === "function" ? value.bind(target) : value;
+            },
+            set(target: { [prop: string]: any }, prop: any, value: any) {
+                target[prop] = value;
+                self.eventBus().emit(Store.EVENTS.FLOW_SDU, { ...target }, target);
+                return true;
+            },
+            deleteProperty() {
+                throw new Error("Нет доступа");
+            }
+        });
+    }
+}
+export default Store
