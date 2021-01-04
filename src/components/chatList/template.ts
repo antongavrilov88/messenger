d2e4da60912ad90b75ae35b39a10fb20ae64bee6
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2021-01-04 23:09:52 +0300

    add updating chat-list after changing chat avatar

--- src/components/chatList/template.ts
+++ src/components/chatList/template.ts
@@ -4,7 +4,7 @@ export const tpl = `<div class="chat-list__container__list">
                             <li class="chat-list__item" id="{{this.id}}">
                                 <hr>
                                 <div class="chat-list__item__avatar__container">
-                                <img src="https://ya-praktikum.tech{{this.avatar}}" class="chat-list__item__avatar__container" />
+                                <img src="https://ya-praktikum.tech{{this.avatar}}" width="47px;" height="47px;" style="border-radius: 23.5px;"/>
                                 </div>
                                 <span class="chat-list__item__chat-author__container">{{this.title}}   <span class="chat-list__delete_button">&#10008;</span></span>
                                 <div class="chat-list__item__last-message__container">
--- src/components/chatUsersList/template.ts
+++ src/components/chatUsersList/template.ts
@@ -5,7 +5,7 @@ export const tpl = `<div class="chat-users-list__container__list">
                             <li class="chat-users-list__item" id="{{this.id}}">
                                 <hr>
                                 <div class="chat-users-list__item__avatar__container">
-                                    <img src="https://ya-praktikum.tech{{this.avatar}}" class="chat-users-list__item__avatar__container" />
+                                    <img src="https://ya-praktikum.tech{{this.avatar}}" width="47px;" height="47px;" style="border-radius: 23.5px;"/>
                                 </div>
                                 <span class="chat-users-list__item__chat-author__container">{{this.first_name}}  {{this.second_name}} <span class="chat-users-list__delete_button">&#10008;</span></span>
                                 <span class="chat-users-list__item__time">{{ this.time }}</span>
@@ -17,7 +17,7 @@ export const tpl = `<div class="chat-users-list__container__list">
                             <li class="chat-users-list__item" id="{{this.id}}">
                                 <hr>
                                 <div class="chat-users-list__item__avatar__container">
-                                    <img src="https://ya-praktikum.tech{{this.avatar}}" class="chat-users-list__item__avatar__container" />
+                                    <img src="https://ya-praktikum.tech{{this.avatar}}" width="47px;" height="47px;" style="border-radius: 23.5px;"/>
                                 </div>
                                 <span class="chat-users-list__item__chat-author__container">{{this.first_name}}  {{this.second_name}} <span class="chat-list__add-user_button">&#10004;</span></span>
                                 <span class="chat-users-list__item__time">{{this.time}}</span>
