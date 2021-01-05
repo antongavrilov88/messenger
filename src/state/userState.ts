36982384da828f071e971fd918b3c82fa93ee956
antongavrilov88@gmail.com <antongavrilov88@gmail.com>
2021-01-05 17:17:59 +0300

    commit scss

--- src/state/userState.ts
+++ src/state/userState.ts
@@ -2,7 +2,6 @@ export const userStore = (state: any) => {
     return {
         ...state,
         user: {
-            token: localStorage.getItem('token') ? localStorage.getItem('token') : null,
             userData: null,
             userID: null,
             error: null
--- static/assets/js/src/state/userState.js
+++ static/assets/js/src/state/userState.js
@@ -1,6 +1,5 @@
 export const userStore = (state) => {
     return Object.assign(Object.assign({}, state), { user: {
-            token: localStorage.getItem('token') ? localStorage.getItem('token') : null,
             userData: null,
             userID: null,
             error: null
